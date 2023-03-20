69b4f728353ae025c3fdf131d6c575bacd851b99
Arihin <arihin.slava@yandex.ru>
2023-03-20 23:05:42 +0300

    Hw Connecting Libraries. Сorrect version

--- src/main/java/ru/receptbook/receiptsbook/model/Recipe.java
+++ src/main/java/ru/receptbook/receiptsbook/model/Recipe.java
@@ -1,12 +1,18 @@
 package ru.receptbook.receiptsbook.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
 public class Recipe {
 
-    public String name;
-    public int cookingTime;
+    private String name;
+    private int cookingTime;
     List<Ingredient> ingredients = new ArrayList<>();
     List<String> cookingSteps = new ArrayList<>();
 
